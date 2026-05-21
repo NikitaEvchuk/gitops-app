9b9f6e2c574ce2ac6755f0cbc45f8d61a7f154cd
Mykyta <nikitaevcuk@gmail.com>
2026-05-21 19:14:02 +0300

    Initial commit: FastAPI app for GitOps project

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {
+        "project": "Cloud-Native CI/CD Pipelines with GitOps",
+        "team": [
+            {
+                "name": "Mykyta Yevchuk",
+                "student_id": "73369"
+            },
+            {
+                "name": "Sialitski Bagdan",
+                "student_id": "71669"
+            }
+        ],
+        "status": "App is running perfectly for both developers!"
+    }
